46819b78f2da6e9f167424a56d1ae36712a1ccf0
Mete Bilgin <mete.bilgin@geniussports.com>
2018-03-11 20:39:32 +0200

    added default password if it is not setuped

--- security.groovy
+++ security.groovy
@@ -5,13 +5,15 @@ import hudson.security.*
 import jenkins.security.s2m.AdminWhitelistRule
 
 def instance = Jenkins.getInstance()
-def env = System.getenv()
 def hudsonRealm = new HudsonPrivateSecurityRealm(false)
-hudsonRealm.createAccount( env['JENKINS_USERNAME'], env['JENKINS_PASSWORD'])
+def adminUsername = System.getenv('JENKINS_USERNAME') ?: 'admin'
+def adminPassword = System.getenv('JENKINS_PASSWORD') ?: 'password'
+
+hudsonRealm.createAccount(adminUsername, adminPassword)
 instance.setSecurityRealm(hudsonRealm)
 
 def strategy = new FullControlOnceLoggedInAuthorizationStrategy()
 instance.setAuthorizationStrategy(strategy)
 instance.save()
-
+jenkins.model.Jenkins.instance.getDescriptor("jenkins.CLI").get().setEnabled(false)
 Jenkins.instance.getInjector().getInstance(AdminWhitelistRule.class).setMasterKillSwitch(false)
