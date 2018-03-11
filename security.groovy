2ac19bbcf411ecb3ea7e9923634c912c0fe84a0f
Mete Bilgin <mete.bilgin@geniussports.com>
2018-03-11 21:29:01 +0200

    removed jnlp protocols for security

--- security.groovy
+++ security.groovy
@@ -4,6 +4,7 @@ import jenkins.model.*
 import hudson.security.*
 import jenkins.security.s2m.AdminWhitelistRule
 
+
 def instance = Jenkins.getInstance()
 def hudsonRealm = new HudsonPrivateSecurityRealm(false)
 def adminUsername = System.getenv('JENKINS_USERNAME') ?: 'admin'
@@ -14,6 +15,10 @@ instance.setSecurityRealm(hudsonRealm)
 
 def strategy = new FullControlOnceLoggedInAuthorizationStrategy()
 instance.setAuthorizationStrategy(strategy)
+instance.getDescriptor("jenkins.CLI").get().setEnabled(false)
+instance.injector.getInstance(AdminWhitelistRule.class).setMasterKillSwitch(false);
+instance.setSlaveAgentPort(-1);
+HashSet<String> newProtocols = new HashSet<>(instance.getAgentProtocols());
+newProtocols.removeAll(Arrays.asList( "JNLP3-connect", "JNLP2-connect", "JNLP-connect", "CLI-connect" ));
+instance.setAgentProtocols(newProtocols);
 instance.save()
-jenkins.model.Jenkins.instance.getDescriptor("jenkins.CLI").get().setEnabled(false)
-Jenkins.instance.getInjector().getInstance(AdminWhitelistRule.class).setMasterKillSwitch(false)
